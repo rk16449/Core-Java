d53ab2c38675749498feab2408b74050cdd7d3f0
Raj <32778886+rk16449@users.noreply.github.com>
2018-06-02 01:12:56 +0100

    diamond notation test

--- Generics/test/TestMultiplePair.java
+++ Generics/test/TestMultiplePair.java
@@ -16,8 +16,25 @@ public class TestMultiplePair {
 		// Due to autoboxing, it is valid to pass a String and an int to the class here
 		iPair<String, Integer> p1 = new MultiplePair<String, Integer>("Test", 5);
 		
-		// Output should be 5
-		assertEquals(p1.getKey(), new Integer(5)); 
+		
+		// key should be 'Test'
+		assertEquals(p1.getKey(), "Test"); 
+		
+		// value should be '5'
+		assertEquals(p1.getValue(), new Integer(5)); 
+	}
+	
+	@Test
+	void testCreateDiamond() {
+		
+		// Java compiler can infer the K and V types so we can use Diamond notation
+		iPair<String, Integer> p1 = new MultiplePair<>("Test", 5);
+		
+		// key should be 'Test'
+		assertEquals(p1.getKey(), "Test"); 
+		
+		// value should be '5'
+		assertEquals(p1.getValue(), new Integer(5)); 
 	}
 
 }
