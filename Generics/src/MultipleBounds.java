a5665907bdbb26983e2b2bc90d24c93340acb1b2
Raj <32778886+rk16449@users.noreply.github.com>
2018-06-02 05:16:34 +0100

    example of multiple bound not working

--- Generics/src/MultipleBounds.java
+++ Generics/src/MultipleBounds.java
@@ -1,6 +1,9 @@
 class A{
 	
 }
+class A2{
+	
+}
 interface B{
 	
 }
@@ -9,7 +12,10 @@ interface C{
 }
 
 
-
+// A must be specified first, one class then multiple interfaces is allowed
 public class MultipleBounds <T extends A & B & C> {
 
 }
+
+
+// public class MultipleBounds <T extends B & C & A> {} // compiler error, A after interfaces
