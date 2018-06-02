deef05da2e3d3288206ea57fa39ce233488a0fe5
Raj <32778886+rk16449@users.noreply.github.com>
2018-06-02 06:32:48 +0100

    constructor generic tests

---
+++ Generics/test/TestGenericClass.java
@@ -0,0 +1,13 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestGenericClass {
+
+	@Test
+	void testConstructor() {
+		GenericClass<Integer> intObject = new GenericClass<>("Constructor");
+		System.out.println(intObject.getVal());
+	}
+
+}
