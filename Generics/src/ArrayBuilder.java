1384abaec8b377874948759802c1645a7a6408e2
Raj <32778886+rk16449@users.noreply.github.com>
2018-06-02 23:48:57 +0100

    updated ArrayBuilder

--- Generics/src/ArrayBuilder.java
+++ Generics/src/ArrayBuilder.java
@@ -1,4 +1,16 @@
+import java.util.*;
 
 public class ArrayBuilder {
-
+	public static <T> void addToList(List<T> list, T...elements) {
+		for(T x: elements) {
+			list.add(x);
+		}
+	}
+	
+	public static void faultyMethod(List<String>...lists) {
+		Object[] objectArr = lists;
+		objectArr[0] = Arrays.asList(73);
+		String s = lists[0].get(0); // ClassCastException thrown here
+		
+	}
 }
