2bc5a354c6a698c359247e94d1156462defce670
Raj <32778886+rk16449@users.noreply.github.com>
2018-06-16 19:50:01 +0100

    added ways of legal variable creation and keywords

---
+++ Primitives/src/LegalNaming.java
@@ -0,0 +1,37 @@
+
+public class LegalNaming {
+
+	public static void main(String[] args) {
+		// Legal variable types
+		int test_a = 0;
+		int test_1 = 1;
+		int $34343 = 2;
+		
+		
+		// Lower precision type byte can be assigned to higher precision type int
+		byte test = 4;
+		int lol = test;
+		
+		 
+		// Variables cannot start with a number
+		/** int 1_asdajkada is illegal */
+		
+		boolean a = false;
+		char c;
+		int i;
+		c = 'A';		// 1
+		i = c;		//2
+		//c = i + 1;	//3 - compilation error here
+		c++;		//4
+		
+		
+		// Valid creation of arrays
+		int[] []x[];
+		
+		
+		// Keywords
+		// transient 
+		
+	}
+	
+}
